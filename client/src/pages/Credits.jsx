743d4202d76159b6ff3a5782a5c566f972eaf3e2
prince <princ3mxd@example.com>
2026-04-20 15:27:27 +0530

    fix(ui): add flex-1 constraint to community and credit layout to prevent overflowing offscreen

--- client/src/pages/Credits.jsx
+++ client/src/pages/Credits.jsx
@@ -60,7 +60,7 @@ const Credits = () => {
   if (loading) return <Loading />;
 
   return (
-    <div className="max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12">
+    <div className="flex-1 w-full max-w-7xl h-screen overflow-y-scroll mx-auto px-4 sm:px-6 lg:px-8 py-12">
       <h2 className="text-3xl font-semibold text-center mb-10 xl:mt-30 text-gray-800 dark:text-white">
         Credit Plans
       </h2>
